4aa2a546ee7ddbabf3c14af5d5de7eae7791b7b3
SzigetiB <szigeti.balazs@blathy.info>
2026-03-22 12:46:35 +0100

    Proeckt alapja

---
+++ MusicAlbums/MusicAlbums/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace MusicAlbums
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
