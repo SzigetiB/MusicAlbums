1d38c90cf1e563e5db378b406d9cdb33313b0cc3
SzigetiB <szigeti.balazs@blathy.info>
2026-03-22 14:15:54 +0100

    Bug fix and AddAlbumInteractive()

--- MusicAlbums/MusicAlbums/obj/Debug/net10.0/MusicAlbums.AssemblyInfo.cs
+++ MusicAlbums/MusicAlbums/obj/Debug/net10.0/MusicAlbums.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MusicAlbums")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+161847fa95bf6dcd6bc5232177fa313946d8b431")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+83f8388437833aec322331efc380546bbcbb7018")]
 [assembly: System.Reflection.AssemblyProductAttribute("MusicAlbums")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MusicAlbums")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
